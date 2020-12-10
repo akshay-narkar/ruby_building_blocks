2a40e8f6bc6e32e326b02349761d418c4d6a8bdf
akshay-narkar <akshaynarkar1295@gmail.com>
2020-12-10 20:25:58 +0530

    Add tests for my_select & my_all?

--- spec/enum_methods_rspec.rb
+++ spec/enum_methods_rspec.rb
@@ -29,3 +29,45 @@ describe Enumerable do
       expect(array.my_each_with_index).to be_kind_of(Enumerator)
     end
   end
+
+  describe '#my_select Goes through each element of the array and returns a new array based on selection' do
+    it 'when block is given' do
+      expect(array.my_select { |x| x > 2 }).to eq(array.select { |x| x > 2 })
+    end
+ 
+    it 'when block is not given' do
+      expect(array.my_select).to be_kind_of(Enumerator)
+    end
+  end
+ 
+  describe '#my_all? Goes through each element of the structure. Returns true or false based on condition provided if
+  all contents satisfy the conditons' do
+    context 'when block is given' do
+      it 'when array is passed' do
+        expect(array.my_all? { |x| x > 2 }).to be_falsy
+      end
+      it 'when hash is passed' do
+        expect(hash1.my_all? { |_x, y| y > 20 }).to be_falsy
+      end
+      it 'array is passed with block requirement as Numeric ' do
+        expect(array.my_all? { |x| x.is_a?(Numeric) }).to be_truthy
+      end
+      it 'array1 is passed with block requirement as Numeric ' do
+        expect(array1.my_all? { |x| x.is_a?(Numeric) }).to be_falsy
+      end
+    end
+ 
+    context ' when block is not given' do
+      it 'when block is not given and argument is given' do
+        expect(array.my_all?(Integer)).to be_truthy
+      end
+ 
+      it 'when block is not given and argument is not given' do
+        expect(array.my_all?).to be_truthy
+      end
+ 
+      it 'when block is not given and argument is not given & array with false is passed' do
+        expect(array1.my_all?).to be_falsy
+      end
+    end
+  end
